b220734df141e4cc22a23db0dde6cc4226c48dbc
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-04-16 00:02:45 -0300

    Implementacao de CRUD para a app empresas, incluindo a vinculacao da empresa com um funcionario pelo login do usuario logado

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView
+from .models import Empresa
 
-# Create your views here.
+
+class EmpresaCreate(CreateView):
+    model = Empresa
+    fields = ['nome']
+
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.empresa = obj
+        funcionario.save()
+        return super().form_valid(form)
+
+
+class EmpresaEdit(UpdateView):
+    model = Empresa
+    fields = ['nome']
+
+
