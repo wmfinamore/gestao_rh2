ec969ad3b0e1a655f358bf9c547d98c9436ffe15
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-04-23 11:46:32 -0300

    Implementada funcionalidade para edição do registro de hora extra à partir do banco de horas do funcionário e a partir da lista geral de banco de horas

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -31,6 +31,20 @@ class HoraExtraEdit(UpdateView):
         return kwargs
 
 
+class HoraExtraEditBase(UpdateView):
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
+    # success_url = reverse_lazy('list_hora_extra')
+
+    def get_success_url(self):
+        return reverse_lazy('update_hora_extra_base', args=[self.object.id])
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraEditBase, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
+
 class HoraExtraDelete(DeleteView):
     model = RegistroHoraExtra
     success_url = reverse_lazy('list_hora_extra')
