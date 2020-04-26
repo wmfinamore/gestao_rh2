26f64b3b2c482b2425f338132787d7701f5db6ff
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-04-25 21:40:55 -0300

    Implementada funcionalidade para geração de relatório PDF da lista de funcionários usando reportlab

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -6,10 +6,12 @@ from .views import (
    FuncionarioCreate,
 )
 
+from .views import relatorio_funcionarios
 
 urlpatterns = [
    path('', FuncionariosList.as_view(), name='list_funcionarios'),
    path('editar/<int:pk>', FuncionarioEdit.as_view(), name='update_funcionario'),
    path('deleter/<int:pk>', FuncionarioDelete.as_view(), name='delete_funcionario'),
    path('novo/', FuncionarioCreate.as_view(), name='create_funcionario'),
+   path('relatorio_funcionarios/', relatorio_funcionarios, name='relatorio_funcionarios'),
 ]
