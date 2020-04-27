f031ab8c859785100c2c10afaa90bf51f28e81ee
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-04-26 21:57:06 -0300

    Implementado exemplo para testar o djangorestframework

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.models import User, Group
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets
+
+from apps.core.serializers import UserSerializer, GroupSerializer
 from apps.funcionarios.models import Funcionario
 
 @login_required
@@ -7,3 +11,13 @@ def home(request):
     data = {}
     data['usuario'] = request.user
     return render(request, 'core/index.html', data)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
--- gestao_rh2/urls.py
+++ gestao_rh2/urls.py
@@ -2,6 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from apps.core.views import GroupViewSet, UserViewSet
+
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
     path('', include('apps.core.urls')),
@@ -12,4 +19,8 @@ urlpatterns = [
     path('horas-extras/', include('apps.registro_hora_extra.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
