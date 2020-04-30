b3379b4dc2d4072b060ca4f6833905f0884a7c00
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-04-30 08:33:01 -0300

    Implementadas melhorias no index para exibição de dashboard

--- gestao_rh2/urls.py
+++ gestao_rh2/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
+# from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 from apps.core.views import GroupViewSet, UserViewSet
@@ -17,9 +17,9 @@ router.register(r'api/horas-extras', RegistroHoraExtraViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-urlpatterns += i18n_patterns(
+# ]
+#
+# urlpatterns += i18n_patterns(
     path('', include('apps.core.urls')),
     path('funcionarios/', include('apps.funcionarios.urls')),
     path('departamentos/', include('apps.departamentos.urls')),
@@ -32,7 +32,7 @@ urlpatterns += i18n_patterns(
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
